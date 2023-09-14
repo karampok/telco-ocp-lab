394fc64732a9478bf8bcf02588edb8b5b54d42e0
karampok <karampok@gmail.com>
2023-09-14 15:07:58 +0200

    more cpu

--- hub-template/day1/install-acm.sh
+++ hub-template/day1/install-acm.sh
@@ -9,6 +9,7 @@ oc apply -f 00-lvm-storage.yaml
 oc apply -f 00-openshift-gitops.yaml
 oc apply -f 00-talm.yaml
 until oc get MultiClusterHub 2>/dev/null; do sleep 30; done
+until oc get LVMCluster 2>/dev/null; do sleep 30; done
 
 oc apply -f 10-provisioning.yaml
 oc apply -f 10-lvm-instance.yaml
