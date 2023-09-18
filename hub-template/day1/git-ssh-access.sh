5cbdeb09f6d2f7c825321f0dd0ba4e43d0e31019
Konstantinos Karampogias <karampok@gmail.com>
2023-09-18 12:52:31 +0200

    Dev (#2)
    * add sinfra bin
    
    * fix Readme
    
    * add k plan
    
    * fix content
    
    * remove bmc dummy secret
    
    * fix install script
    
    * fix install script two
    
    * add newer binary
    
    * do the install acm on day2
    
    * rename day1 versus day2
    
    * relayout day1
    
    * increase memory
    
    * add script
    
    * more cpu
    
    * remove igb
    
    * add a bit more of cpus
    
    * fix rhel 9.2 doc
    
    * add github action file

---
+++ hub-template/day1/git-ssh-access.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euoE pipefail
+
+REPO=${GITREPO:-karampok/telco-ocp-lab.git}
+KEY=${GITKEY_FILE:-.github-argo}
+
+if [[ ! -f $KEY ]]
+then
+  ssh-keygen -q -t ed25519 -C "github-repo" -f "$KEY" <<< $'\ny' >/dev/null 2>&1
+fi
+
+oc create -n openshift-gitops secret generic ztp-spokes-repo \
+     --from-file=sshPrivateKey="$KEY" \
+     --from-literal=type=git \
+     --from-literal=url=git@github.com:"$REPO" \
+     --from-literal=insecure=true
+
+oc label -n openshift-gitops secret ztp-spokes-repo argocd.argoproj.io/secret-type=repository
+
+echo "sshPrivateKey has been added into ArgoCD repository."
+echo
+echo "Then go to https://github.com/${REPO%".git"}/settings/keys"
+echo "Add a new deploy key"
+echo "Title: argocd"
+echo "Key: $(cat "$KEY.pub")"
+echo
+# or
+# argocd login openshift-gitops-server-openshift-gitops.apps.x.com --sso"
+# argocd repo add git@github.com:$REPO --ssh-private-key-path /tmp/github-ed25519 --insecure --insecure-ignore-host-key"
+# argocd repo list
---
+++ hub-template/day1/install-ztp.sh
@@ -0,0 +1,78 @@
+#! /usr/bin/env bash
+set -euoE pipefail
+
+out=/tmp/out
+mkdir -p "$out"
+
+PULL_SECRET_PATH=${PULL_SECRET_PATH:-.pull-secret.json}
+podman run --log-driver=none --rm --authfile="$PULL_SECRET_PATH" \
+  registry.redhat.io/openshift4/ztp-site-generate-rhel8:v4.13 extract /home/ztp/argocd/deployment --tar | tar x -C "$out"
+
+# or
+# git clone -b release-4.13 git@github.com:openshift-kni/cnf-features-deploy.git /tmp/out
+# cp -r /tmp/ou2/ztp/gitops-subscriptions/argocd/deployment "$out" 
+
+oc patch argocd openshift-gitops -n openshift-gitops --type=merge \
+  --patch-file "$out"/argocd-openshift-gitops-patch.json
+
+# increase memory/cpu request/limits
+# kustomizeBuildOptions: --enable-alpha-plugins
+# add init-container
+
+REPO=${GITREPO:-karampok/telco-ocp-lab.git}
+BRANCH=${BRANCH:-gitops}
+echo "---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: clusters-sub
+---
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: clusters
+  namespace: openshift-gitops
+spec:
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: clusters-sub
+  project: ztp-app-project
+  source:
+    path: ztp-spokes/sites
+    repoURL: git@github.com:$REPO
+    targetRevision: $BRANCH
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    syncOptions:
+      - CreateNamespace=true" > "$out"/clusters-app.yaml
+
+echo "---
+apiVersion: v1
+kind: Namespace
+metadata:
+  name: policies-sub
+---
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: policies
+  namespace: openshift-gitops
+spec:
+  destination:
+    server: https://kubernetes.default.svc
+    namespace: policies-sub
+  project: policy-app-project
+  source:
+    path: ztp-spokes/policies
+    repoURL: git@github.com:$REPO
+    targetRevision: $BRANCH
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    syncOptions:
+      - CreateNamespace=true" > "$out"/policies-app.yaml
+
+oc apply -k "$out"/
