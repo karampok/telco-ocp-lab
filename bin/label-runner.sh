91ab7c28e2093897335013a378f50e3bcff20933
karampok <karampok@gmail.com>
2023-11-13 15:38:11 +0100

    try red green runner label two

--- bin/label-runner.sh
+++ bin/label-runner.sh
@@ -7,6 +7,12 @@ RUNNER_ID=$(curl -sS -H "Authorization: Bearer $GH_TOKEN" \
               "https://api.github.com/repos/karampok/telco-ocp-lab/actions/runners" | \
               jq --arg RN "$RUNNER" -r '.runners[] | select(.name == $RN) | .id')
 
+curl -X DELETE \
+    -s --noproxy "*"  -w "%{http_code} %{url_effective}\\n" -L --globoff \
+    -H "Authorization: Bearer $GH_TOKEN" \
+    -H "Accept: application/vnd.github.v3+json" \
+    "https://api.github.com/repos/karampok/telco-ocp-lab/actions/runners/${RUNNER_ID}/labels/green" || true
+
 curl -X POST \
      -s --noproxy "*"  -w "%{http_code} %{url_effective}\\n" -L --globoff \
      -H "Authorization: Bearer $GH_TOKEN" \
@@ -14,9 +20,3 @@ curl -X POST \
      -H "Content-Type: application/json" \
      -d '{"labels": ["'"$1"'"]}' \
      "https://api.github.com/repos/karampok/telco-ocp-lab/actions/runners/${RUNNER_ID}/labels"
-
-curl -X DELETE \
-    -s --noproxy "*"  -w "%{http_code} %{url_effective}\\n" -L --globoff \
-    -H "Authorization: Bearer $GH_TOKEN" \
-    -H "Accept: application/vnd.github.v3+json" \
-    "https://api.github.com/repos/karampok/telco-ocp-lab/actions/runners/${RUNNER_ID}/labels/green"
