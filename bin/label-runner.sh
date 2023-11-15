73b35e97ec482ae6768d859b960377c21f0c1824
karampok <karampok@gmail.com>
2023-11-15 10:35:22 +0100

    remove worker 1 and improve runner labeling

--- bin/label-runner.sh
+++ bin/label-runner.sh
@@ -1,22 +1,55 @@
 #!/bin/bash
 set -euoE pipefail
 
-# Get the runner's ID
+runners=$(curl -sS -H "Authorization: Bearer $GH_TOKEN" \
+              -H "Accept: application/vnd.github.v3+json" \
+              "https://api.github.com/repos/karampok/telco-ocp-lab/actions/runners")
+
+if [ $# -eq 0 ]; then
+  echo "$runners" | jq -r '.runners[] | "\(.name) \(.labels | map(.name) | join(" "))"'
+  exit 0
+fi
+
+RUNNER=$1
+shift
+
 RUNNER_ID=$(curl -sS -H "Authorization: Bearer $GH_TOKEN" \
               -H "Accept: application/vnd.github.v3+json" \
               "https://api.github.com/repos/karampok/telco-ocp-lab/actions/runners" | \
               jq --arg RN "$RUNNER" -r '.runners[] | select(.name == $RN) | .id')
 
-curl -X DELETE \
-    -s --noproxy "*"  -w "%{http_code} %{url_effective}\\n" -L --globoff \
-    -H "Authorization: Bearer $GH_TOKEN" \
-    -H "Accept: application/vnd.github.v3+json" \
-    "https://api.github.com/repos/karampok/telco-ocp-lab/actions/runners/${RUNNER_ID}/labels/green" || true
+add_label() {
+  curl -X POST \
+      -s --noproxy "*"  -w "%{http_code} $RUNNER %{url_effective}\\n" -L --globoff -o /dev/null \
+      -H "Authorization: Bearer $GH_TOKEN" \
+      -H "Accept: application/vnd.github.v3+json" \
+      -H "Content-Type: application/json" \
+      -d '{"labels": ["'"$2"'"]}' \
+      "https://api.github.com/repos/karampok/telco-ocp-lab/actions/runners/${1}/labels"
+}
 
-curl -X POST \
-     -s --noproxy "*"  -w "%{http_code} %{url_effective}\\n" -L --globoff \
-     -H "Authorization: Bearer $GH_TOKEN" \
-     -H "Accept: application/vnd.github.v3+json" \
-     -H "Content-Type: application/json" \
-     -d '{"labels": ["'"$1"'"]}' \
-     "https://api.github.com/repos/karampok/telco-ocp-lab/actions/runners/${RUNNER_ID}/labels"
+delete_label() {
+  curl -X DELETE \
+      -s --noproxy "*"  -w "%{http_code} $RUNNER %{url_effective}\\n" -L --globoff -o /dev/null \
+      -H "Authorization: Bearer $GH_TOKEN" \
+      -H "Accept: application/vnd.github.v3+json" \
+      "https://api.github.com/repos/karampok/telco-ocp-lab/actions/runners/${1}/labels/${2}"
+}
+
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        +*)
+            add_label "$RUNNER_ID" "${1:1}"
+            ;;
+        -*)
+            delete_label "$RUNNER_ID" "${1:1}"
+            ;;
+        *)
+            add_label "$RUNNER_ID" "${1}"
+            ;;
+    esac
+    shift
+done
+
+
