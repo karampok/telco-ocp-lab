8080a1fcbb0944acd938ba0dea9d8c3ed7be1f19
karampok <karampok@gmail.com>
2023-11-22 15:32:32 +0100

    sync to dev

--- deploy-ocp.sh
+++ deploy-ocp.sh
@@ -4,17 +4,11 @@ set -euoE pipefail
 openshift-install version
 
 cp -r sno-template /share/hub
-#read -p -r # tree before,after
 
 PULL_SECRET=$(jq '.' -c "${PULL_SECRET_PATH:-.pull-secret.json}") #one liner
-SSHPUBKEY=$(cat "${SSHPUBKEYFILE:-.id-rsa.pub}")
-sed -i "s|SSHPUBKEY|$SSHPUBKEY|" /share/hub/install-config.yaml
 sed -i "s/PULLSECRET/$PULL_SECRET/g" /share/hub/install-config.yaml
 
 openshift-install agent create image --log-level info  --dir /share/hub
-
-#read -p -r # discovery ISO created // nmstatectl required
-#read -p -r  # python3 -m http.server 9000 -d /share
 
 node="https://192.168.100.100:8000/redfish/v1/Systems/11111111-1111-1111-1111-111111111115"
 source ./redfish-actions/sushy.sh
