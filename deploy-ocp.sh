6e23ac0abb0534cbd49f24ffd96edd3b4200da76
karampok <karampok@gmail.com>
2024-03-14 13:07:11 +0100

    start agent-based lab

--- deploy-ocp.sh
+++ deploy-ocp.sh
@@ -3,24 +3,26 @@ set -euoE pipefail
 
 openshift-install version
 
-cp -r sno-template /share/hub
+name=${1}   #hpe183,sno,5gc
+folder=${folder:-"/share/${name}"}
+cp -r "${name}"-template "${folder}"
 
 PULL_SECRET=$(jq '.' -c "${PULL_SECRET_PATH:-.pull-secret.json}") #one liner
-sed -i "s/PULLSECRET/$PULL_SECRET/g" /share/hub/install-config.yaml
+sed -i "s/PULLSECRET/$PULL_SECRET/g" "${folder}"/install-config.yaml
 
-openshift-install agent create image --log-level info  --dir /share/hub
+openshift-install agent create image --log-level info  --dir "${folder}"
 
-node="https://192.168.100.100:8000/redfish/v1/Systems/11111111-1111-1111-1111-111111111115"
 source ./redfish-actions/sushy.sh
-power_off "$node"
-media_eject "$node"
-media_insert "$node" http://10.10.20.200:9000/hub/agent.x86_64.iso
-boot_once "$node" #dummy in sushy
-power_on "$node"
+for node in $(cat "${folder}"/bmc-hosts);
+do
+  power_off "$node"
+  media_eject "$node"
+  media_insert "$node" "${HTTP_SERVER:-http://192.168.100.200:9000}"/"${name}"/agent-"${name}".iso
+  boot_once "$node"
+  power_on "$node"
+done
 
-
-mkdir -p ~/.kube && cp /share/hub/auth/kubeconfig ~/.kube/config
+mkdir -p ~/.kube && cp "${folder}"/auth/kubeconfig ~/.kube/config
 cat << EOF
-export KUBECONFIG=/share/hub/auth/kubeconfig
 openshift-install agent wait-for install-complete --log-level info --dir /share/hub
 EOF
