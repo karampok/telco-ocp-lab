31bb5e8c3e3e2f862d391a22c245f7fae5b654cc
karampok <karampok@gmail.com>
2024-03-19 16:38:51 +0100

    fix the frr config

--- pkg/run-ipforwarding.go
+++ pkg/run-ipforwarding.go
@@ -7,6 +7,8 @@ import (
 // MTU ISSUE
 var runIPForwarding = `
 export KUBECONFIG=/home/kka/.kube/ztp5gc.yaml
+green-in and default route on 11.11.11.118
+red-in and default route on a node 12.12.12.118
 `
 
 func RunIPForwardingDemo() *Run {
