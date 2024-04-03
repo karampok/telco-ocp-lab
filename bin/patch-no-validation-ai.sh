8caf4e1bf568ffd87fd89c58857fe6a961157369
karampok <karampok@gmail.com>
2024-04-03 11:25:58 +0200

    workaround ipv6 bug

---
+++ bin/patch-no-validation-ai.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+set -xeuoE pipefail
+
+AI_URL=${1:-"10.10.10.116:8090"}
+url="http://$AI_URL/api/assisted-install/v2/clusters/"
+until ret=$(curl -s -o /dev/null -w "%{http_code}" "$url"); do
+  if [ "$ret" -eq 200 ]; then
+      break
+  fi
+  sleep 30;
+done
+
+CLUSTER_ID=$(curl "$url" | jq '.[0].id' | tr -d '"')
+curl -X PUT -H "Content-Type: application/json" \
+         -d '{"host-validation-ids": "[\"belongs-to-majority-group\"]"}' \
+         "$URL"/"$CLUSTER_ID"/ignored-validations
