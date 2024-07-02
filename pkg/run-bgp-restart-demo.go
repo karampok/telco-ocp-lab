a5b25b250602f12f457d686af7edc33d3f8b5ef4
karampok <karampok@gmail.com>
2024-07-02 15:33:25 +0200

    simplifly setup and use custom images

--- pkg/run-bgp-restart-demo.go
+++ pkg/run-bgp-restart-demo.go
@@ -12,40 +12,17 @@ import (
 func RunBGPGracefulRestart() *Run {
 	r := NewRun("Run BPG graceful restart demo")
 
-	c := `kubectl apply -f day2/blue-peering.yaml
+	c := `
 kubectl apply -f day2/green-peering.yaml
 kubectl apply -f day2/red-peering.yaml`
 	r.Step(S("Setup peering"), S(c))
 
-	d := `kubectl apply -f day2/blue-pod-one.yaml
-kubectl apply -f day2/green-pod-one.yaml
-kubectl apply -f day2/red-pod-one.yaml`
-	r.Step(S("Deploy workloads"), S(d))
-
-	c = `kubectl get pods -o wide; kubectl get svc`
-	r.Step(S("Verify workload"), S(c))
-
-	c = `tmux new-window -n clients
-tmux send-keys -t clients.0 "docker exec -it clab-vlab-sidecar-gw1 /bin/bash" C-m C-m C-m
-tmux send-keys -t clients.0 "curl -sf http://4.4.4.1:5555/hostname" C-m C-m C-m
-tmux send-keys -t clients.0 "while true;do curl -sf http://4.4.4.1:5555/hostname --connect-timeout 1  -o /dev/null || printf \"%s \" \$(date +%s) ;sleep 1;done" C-m
-tmux split-window -v -t clients
-tmux send-keys -t clients.1 "docker exec -it clab-vlab-sidecar-gw1 /bin/bash" C-m C-m C-m
-tmux send-keys -t clients.1 "ip vrf exec green /bin/bash" C-m C-m C-m
-tmux send-keys -t clients.1 "curl -sf http://5.5.5.1:5555/hostname" C-m C-m C-m
-tmux send-keys -t clients.1 "while true;do curl -sf http://5.5.5.1:5555/hostname --connect-timeout 1  -o /dev/null || printf \"%s \" \$(date +%s) ;sleep 1;done" C-m
-tmux split-window -v -t clients
-tmux send-keys -t clients.2 "docker exec -it clab-vlab-sidecar-gw1 /bin/bash" C-m C-m C-m
-tmux send-keys -t clients.2 "ip vrf exec red /bin/bash" C-m C-m C-m
-tmux send-keys -t clients.2 "curl -sf http://6.6.6.1:5555/hostname" C-m C-m C-m
-tmux send-keys -t clients.2 "while true;do curl -sf http://6.6.6.1:5555/hostname --connect-timeout 1  -o /dev/null || printf \"%s \" \$(date +%s) ;sleep 1;done" C-m
-tmux select-layout -t clients even-vertical
-tmux select-window -t clients; tmux setw synchronize-panes on
-`
-	r.Step(nil, S(c))
-
 	c = `tmux new-window -n gateway
-tmux send-keys -t gateway.0 "docker exec clab-vlab-gw1 vtysh -c 'show bgp vrf all summary'" C-m C-m C-m
+tmux send-keys -t gateway.0 "docker exec clab-vlab-gw1 vtysh -c 'show bgp vrf green summary'" C-m C-m C-m
+tmux send-keys -t gateway.0 "docker exec clab-vlab-gw1 vtysh -c 'show bgp vrf red summary'" C-m C-m C-m
+tmux send-keys -t gateway.0 "docker exec clab-vlab-gw1 vtysh -c 'show bfd peers brief'" C-m C-m C-m
+tmux send-keys -t gateway.0 "docker exec clab-vlab-gw1 vtysh -c 'show bgp vrf green neighbors 11.11.11.103' 2>&1 >/tmp/green" C-m C-m C-m
+tmux send-keys -t gateway.0 "docker exec clab-vlab-gw1 vtysh -c 'show bgp vrf red neighbors 12.12.12.103' 2>&1 >/tmp/red" C-m C-m C-m
 tmux send-keys -t gateway.0 "watch -d -c -n 1 docker exec clab-vlab-gw1 vtysh -c \\\"show ip bgp vrf all\\\""
 # tmux send-keys -t gateway.0 "bash" C-m C-m
 # tmux send-keys -t gateway.0 "while true; do docker exec clab-vlab-gw1 vtysh -c 'show ip bgp vrf all'|grep 4.4.4.1 ; echo \"== \$(date --utc)\" ; sleep 1; done" C-m
@@ -56,8 +33,33 @@ tmux send-keys -t gateway.2 "kubectl -n metallb-system logs -c reloader (kubectl
 tmux split-window -v -t gateway
 tmux send-keys -t gateway.3 "kubectl set image daemonset/speaker frr=quay.io/frrouting/frr:9.1.0 -n metallb-system; kubectl -n metallb-system get pods -o wide -w"
 `
+	r.Step(S("Verify peering"), S(c))
 
+	d := `
+kubectl apply -f day2/green-pod-two.yaml
+kubectl apply -f day2/red-pod-two.yaml`
+	r.Step(S("Deploy workloads"), S(d))
+
+	c = `kubectl get pods -o wide; kubectl get svc`
+	r.Step(S("Verify workload"), S(c))
+
+	c = `tmux new-window -n clients
+tmux send-keys -t clients.0 "docker exec -it clab-vlab-gw1-sidecar /bin/bash" C-m C-m C-m
+tmux send-keys -t clients.0 "ip vrf exec green /bin/bash" C-m C-m C-m
+tmux send-keys -t clients.0 "curl -sf http://5.5.5.1:5555/hostname" C-m C-m C-m
+tmux send-keys -t clients.0 "curl -sf \"http://5.5.5.1:5555/shell?cmd=\"env%7Cgrep%20-i%20node%0A\"\"" C-m C-m C-m
+tmux send-keys -t clients.0 "while true;do curl -sf http://5.5.5.1:5555/hostname --connect-timeout 1  -o /dev/null || printf \"%s \" \$(date +%s) ;sleep 1;done" C-m
+tmux split-window -v -t clients
+tmux send-keys -t clients.1 "docker exec -it clab-vlab-gw1-sidecar /bin/bash" C-m C-m C-m
+tmux send-keys -t clients.1 "ip vrf exec red /bin/bash" C-m C-m C-m
+tmux send-keys -t clients.1 "curl -sf http://6.6.6.1:5555/hostname" C-m C-m C-m
+tmux send-keys -t clients.1 "curl -sf \"http://6.6.6.1:5555/shell?cmd=\"env%7Cgrep%20-i%20node%0A\"\"" C-m C-m C-m
+tmux send-keys -t clients.1 "while true;do curl -sf http://6.6.6.1:5555/hostname --connect-timeout 1  -o /dev/null || printf \"%s \" \$(date +%s) ;sleep 1;done" C-m
+tmux select-layout -t clients even-vertical
+tmux select-window -t clients; tmux setw synchronize-panes on
+`
 	r.Step(nil, S(c))
+
 	return r
 }
 
@@ -71,7 +73,7 @@ func RunBGPGracefulRestartWithBFD() *Run {
 	r.Step(S("Deploy workloads"), S(d))
 
 	c = `tmux new-window -n clients
-tmux send-keys -t clients.0 "docker exec -it clab-vlab-sidecar-gw1 /bin/bash" C-m C-m C-m
+tmux send-keys -t clients.0 "docker exec -it clab-vlab-gw1-sidecar /bin/bash" C-m C-m C-m
 tmux send-keys -t clients.0 "ip vrf exec red /bin/bash" C-m C-m C-m
 tmux send-keys -t clients.0 "curl -sf http://6.6.6.1:5555/hostname" C-m C-m C-m
 tmux send-keys -t clients.0 "while true;do curl -sf http://6.6.6.1:5555/hostname --connect-timeout 1 || printf \"%s \" \$(date +%s) ;sleep 1;echo; done" C-m
