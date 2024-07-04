7d7915cde9645dbf584158cc39c5fdc0d638a694
karampok <karampok@gmail.com>
2024-07-04 09:39:40 +0200

    fix ip address on w0 w1

--- pkg/run-bgp-restart-demo.go
+++ pkg/run-bgp-restart-demo.go
@@ -51,10 +51,22 @@ tmux send-keys -t clients.0 "curl -sf \"http://5.5.5.1:5555/shell?cmd=\"env%7Cgr
 tmux send-keys -t clients.0 "while true;do curl -sf http://5.5.5.1:5555/hostname --connect-timeout 1  -o /dev/null || printf \"%s \" \$(date +%s) ;sleep 1;done" C-m
 tmux split-window -v -t clients
 tmux send-keys -t clients.1 "docker exec -it clab-vlab-gw1-sidecar /bin/bash" C-m C-m C-m
-tmux send-keys -t clients.1 "ip vrf exec red /bin/bash" C-m C-m C-m
-tmux send-keys -t clients.1 "curl -sf http://6.6.6.1:5555/hostname" C-m C-m C-m
-tmux send-keys -t clients.1 "curl -sf \"http://6.6.6.1:5555/shell?cmd=\"env%7Cgrep%20-i%20node%0A\"\"" C-m C-m C-m
-tmux send-keys -t clients.1 "while true;do curl -sf http://6.6.6.1:5555/hostname --connect-timeout 1  -o /dev/null || printf \"%s \" \$(date +%s) ;sleep 1;done" C-m
+tmux send-keys -t clients.1 "ip vrf exec green /bin/bash" C-m C-m C-m
+tmux send-keys -t clients.1 "curl -sf http://5.5.5.2:5555/hostname" C-m C-m C-m
+tmux send-keys -t clients.1 "curl -sf \"http://5.5.5.2:5555/shell?cmd=\"env%7Cgrep%20-i%20node%0A\"\"" C-m C-m C-m
+tmux send-keys -t clients.1 "while true;do curl -sf http://5.5.5.2:5555/hostname --connect-timeout 1  -o /dev/null || printf \"%s \" \$(date +%s) ;sleep 1;done" C-m
+tmux split-window -v -t clients
+tmux send-keys -t clients.2 "docker exec -it clab-vlab-gw1-sidecar /bin/bash" C-m C-m C-m
+tmux send-keys -t clients.2 "ip vrf exec red /bin/bash" C-m C-m C-m
+tmux send-keys -t clients.2 "curl -sf http://6.6.6.1:5555/hostname" C-m C-m C-m
+tmux send-keys -t clients.2 "curl -sf \"http://6.6.6.1:5555/shell?cmd=\"env%7Cgrep%20-i%20node%0A\"\"" C-m C-m C-m
+tmux send-keys -t clients.2 "while true;do curl -sf http://6.6.6.1:5555/hostname --connect-timeout 1  -o /dev/null || printf \"%s \" \$(date +%s) ;sleep 1;done" C-m
+tmux split-window -v -t clients
+tmux send-keys -t clients.3 "docker exec -it clab-vlab-gw1-sidecar /bin/bash" C-m C-m C-m
+tmux send-keys -t clients.3 "ip vrf exec red /bin/bash" C-m C-m C-m
+tmux send-keys -t clients.3 "curl -sf http://6.6.6.2:5555/hostname" C-m C-m C-m
+tmux send-keys -t clients.3 "curl -sf \"http://6.6.6.2:5555/shell?cmd=\"env%7Cgrep%20-i%20node%0A\"\"" C-m C-m C-m
+tmux send-keys -t clients.3 "while true;do curl -sf http://6.6.6.2:5555/hostname --connect-timeout 1  -o /dev/null || printf \"%s \" \$(date +%s) ;sleep 1;done" C-m
 tmux select-layout -t clients even-vertical
 tmux select-window -t clients; tmux setw synchronize-panes on
 `
