f5e61eeab12c192c1057b1aed70ae66b6a86d1a8
karampok <karampok@gmail.com>
2025-03-17 16:42:18 +0100

    update 4.18
    Signed-off-by: karampok <karampok@gmail.com>

--- pkg/run-bgp-restart-demo.go
+++ pkg/run-bgp-restart-demo.go
@@ -20,54 +20,35 @@ kubectl apply -f day2/red-peering.yaml`
 	c = `tmux new-window -n gateway
 tmux send-keys -t gateway.0 "docker exec clab-vlab-gw1 vtysh -c 'show bgp vrf green summary'" C-m C-m C-m
 tmux send-keys -t gateway.0 "docker exec clab-vlab-gw1 vtysh -c 'show bgp vrf red summary'" C-m C-m C-m
-tmux send-keys -t gateway.0 "docker exec clab-vlab-gw1 vtysh -c 'show bfd peers brief'" C-m C-m C-m
 tmux send-keys -t gateway.0 "docker exec clab-vlab-gw1 vtysh -c 'show bgp vrf green neighbors 11.11.11.103' 2>&1 >/tmp/green" C-m C-m C-m
 tmux send-keys -t gateway.0 "docker exec clab-vlab-gw1 vtysh -c 'show bgp vrf red neighbors 12.12.12.103' 2>&1 >/tmp/red" C-m C-m C-m
 tmux send-keys -t gateway.0 "watch -d -c -n 1 docker exec clab-vlab-gw1 vtysh -c \\\"show ip bgp vrf all\\\""
-# tmux send-keys -t gateway.0 "bash" C-m C-m
-# tmux send-keys -t gateway.0 "while true; do docker exec clab-vlab-gw1 vtysh -c 'show ip bgp vrf all'|grep 4.4.4.1 ; echo \"== \$(date --utc)\" ; sleep 1; done" C-m
-tmux split-window -h -t gateway
-tmux send-keys -t gateway.1 "docker exec clab-vlab-gw1 tail -f /tmp/frr.log" C-m
 tmux split-window -v -t gateway
-tmux send-keys -t gateway.2 "kubectl -n metallb-system logs -c reloader (kubectl -n metallb-system get pods -l component=speaker -o name) -f" C-m
-tmux split-window -v -t gateway
-tmux send-keys -t gateway.3 "kubectl set image daemonset/speaker frr=quay.io/frrouting/frr:9.1.0 -n metallb-system; kubectl -n metallb-system get pods -o wide -w"
+tmux send-keys -t gateway.1 "# kubectl -n metallb-system delete pods -l component=speaker ; kubectl -n metallb-system get pods -o wide -w"
 `
 	r.Step(S("Verify peering"), S(c))
 
 	d := `
-kubectl apply -f day2/green-pod-one.yaml
-kubectl apply -f day2/red-pod-one.yaml`
+kubectl apply -f day2/gr-demo.yaml
+sleep 5
+kubectl get pods -o wide; kubectl get svc
+`
 	r.Step(S("Deploy workloads"), S(d))
 
-	c = `kubectl get pods -o wide; kubectl get svc`
-	r.Step(S("Verify workload"), S(c))
-
 	c = `tmux new-window -n clients
-tmux send-keys -t clients.0 "docker exec -it clab-vlab-gw1-sidecar /bin/bash" C-m C-m C-m
-tmux send-keys -t clients.0 "ip vrf exec green /bin/bash" C-m C-m C-m
-tmux send-keys -t clients.0 "curl -sf http://5.5.5.1:5555/hostname" C-m C-m C-m
+tmux send-keys -t clients.0 "docker run -it --privileged --rm --net=container:clab-vlab-gw1  quay.io/karampok/snife /bin/bash" C-m
+tmux send-keys -t clients.0 "clear" C-m
+tmux send-keys -t clients.0 "ip vrf exec green /bin/bash" C-m
+tmux send-keys -t clients.0 "curl -sf http://5.5.5.1:5555/hostname" C-m
 tmux send-keys -t clients.0 "curl -sf \"http://5.5.5.1:5555/shell?cmd=\"env%7Cgrep%20-i%20node%0A\"\"" C-m C-m C-m
 tmux send-keys -t clients.0 "while true;do curl -sf http://5.5.5.1:5555/hostname --connect-timeout 1  -o /dev/null || printf \"%s \" \$(date +%s) ;sleep 1;done" C-m
-#while true;do curl -sf http://6.6.6.2:5555/shell?cmd="env%7Cgrep%20-i%20node%0A" --connect-timeout 1 >>/tmp/downtime-6662 || printf "%s " $(date +%s) >> /tmp/downtime-6662;sleep 1;echo >>/tmp/downtime-6662;done
 tmux split-window -v -t clients
-tmux send-keys -t clients.1 "docker exec -it clab-vlab-gw1-sidecar /bin/bash" C-m C-m C-m
-tmux send-keys -t clients.1 "ip vrf exec green /bin/bash" C-m C-m C-m
-tmux send-keys -t clients.1 "curl -sf http://5.5.5.2:5555/hostname" C-m C-m C-m
-tmux send-keys -t clients.1 "curl -sf \"http://5.5.5.2:5555/shell?cmd=\"env%7Cgrep%20-i%20node%0A\"\"" C-m C-m C-m
-tmux send-keys -t clients.1 "while true;do curl -sf http://5.5.5.2:5555/hostname --connect-timeout 1  -o /dev/null || printf \"%s \" \$(date +%s) ;sleep 1;done" C-m
-tmux split-window -v -t clients
-tmux send-keys -t clients.2 "docker exec -it clab-vlab-gw1-sidecar /bin/bash" C-m C-m C-m
-tmux send-keys -t clients.2 "ip vrf exec red /bin/bash" C-m C-m C-m
-tmux send-keys -t clients.2 "curl -sf http://6.6.6.1:5555/hostname" C-m C-m C-m
-tmux send-keys -t clients.2 "curl -sf \"http://6.6.6.1:5555/shell?cmd=\"env%7Cgrep%20-i%20node%0A\"\"" C-m C-m C-m
-tmux send-keys -t clients.2 "while true;do curl -sf http://6.6.6.1:5555/hostname --connect-timeout 1  -o /dev/null || printf \"%s \" \$(date +%s) ;sleep 1;done" C-m
-tmux split-window -v -t clients
-tmux send-keys -t clients.3 "docker exec -it clab-vlab-gw1-sidecar /bin/bash" C-m C-m C-m
-tmux send-keys -t clients.3 "ip vrf exec red /bin/bash" C-m C-m C-m
-tmux send-keys -t clients.3 "curl -sf http://6.6.6.2:5555/hostname" C-m C-m C-m
-tmux send-keys -t clients.3 "curl -sf \"http://6.6.6.2:5555/shell?cmd=\"env%7Cgrep%20-i%20node%0A\"\"" C-m C-m C-m
-tmux send-keys -t clients.3 "while true;do curl -sf http://6.6.6.2:5555/hostname --connect-timeout 1  -o /dev/null || printf \"%s \" \$(date +%s) ;sleep 1;done" C-m
+tmux send-keys -t clients.1 "docker run -it --privileged --rm --net=container:clab-vlab-gw1  quay.io/karampok/snife /bin/bash" C-m
+tmux send-keys -t clients.1 "clear" C-m
+tmux send-keys -t clients.1 "ip vrf exec red /bin/bash" C-m
+tmux send-keys -t clients.1 "curl -sf http://6.6.6.1:6666/hostname" C-m
+tmux send-keys -t clients.1 "curl -sf \"http://6.6.6.1:6666/shell?cmd=\"env%7Cgrep%20-i%20node%0A\"\"" C-m C-m C-m
+tmux send-keys -t clients.1 "while true;do curl -sf http://6.6.6.1:6666/hostname --connect-timeout 1  -o /dev/null || printf \"%s \" \$(date +%s) ;sleep 1;done" C-m
 tmux select-layout -t clients even-vertical
 tmux select-window -t clients; tmux setw synchronize-panes on
 `
@@ -75,3 +56,8 @@ tmux select-window -t clients; tmux setw synchronize-panes on
 
 	return r
 }
+
+// while true
+//     curl -sf http://4.4.4.1:4444/hostname --connect-timeout 1 -o /dev/null; or printf "%s " (date +%s)
+//     sleep 1
+// end
