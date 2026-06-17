a6bed9e24428b4ab72554875c738d79cbbe930a2
Konstantinos Karampogias <karampok@gmail.com>
2026-06-17 14:12:49 +0200

    Fix the ztp approach

--- deploy-ocp.sh
+++ deploy-ocp.sh
@@ -1,23 +1,14 @@
 #! /usr/bin/env bash
 set -euoE pipefail
 
-PULL_SECRET=${PULL_SECRET:-~/.pull-secret.json}
-OCP_RELEASE=${OCP_RELEASE:-"quay.io/openshift-release-dev/ocp-release:4.22.0-rc.5-x86_64"}
-
-oc adm release extract --registry-config "${PULL_SECRET}" \
-  --command=openshift-install --to "${HOME}/.local/bin/" "$OCP_RELEASE"
-openshift-install version
-
-
-name=${1:-mno} #mno,sno,5gc
-folder=${folder:-"/share/${name}"}
+name=${1:-sno} #mno,sno,5gc
+folder=${folder:-"/tmp/${name}"}
 cp -r "${name}"-template "${folder}"
 
 sed -i "s|PULLSECRET|$(jq '.' -c "$PULL_SECRET")|g" "${folder}"/install-config.yaml
+sed -i "s|SSHKEY|$(cat ~/.ssh/authorized_keys)|g" "${folder}"/install-config.yaml
 
 openshift-install agent create image --log-level info --dir "${folder}"
-
-echo "ISO: curl -O ${HTTP_SERVER:-http://10.10.20.200:9000}/${name}/agent.x86_64.iso"
 
 source "${HOME}/sushy.sh"
 while IFS= read -r node; do
@@ -30,18 +21,3 @@ done <"${folder}/bmc-hosts"
 
 mkdir -p ~/.kube && cp "${folder}"/auth/kubeconfig ~/.kube/config
 openshift-install agent wait-for install-complete --log-level info --dir /share/"${name}"
-
-cat <<EOF
-oc patch network.operator cluster -p '{"spec":{"defaultNetwork":{"ovnKubernetesConfig":{"gatewayConfig":{"routingViaHost": true}}}}}' --type=merge
-oc patch network.operator cluster --type=merge --patch-file day1/network-operator-patch.yaml
-oc patch OperatorHub cluster --type merge --patch-file day1/operatorhub-patch.yaml
-# Local registry is needed
-# https://docs.openshift.com/container-platform/4.15/registry/configuring_registry_storage/configuring-registry-storage-baremetal.html
-oc patch configs.imageregistry cluster --type=merge --patch-file day1/image-registry-patch.yaml
-# twice to remove any topologySpreadConstraints: []
-oc patch configs.imageregistry cluster --type=merge --patch-file day1/image-registry-patch.yaml
-oc patch OperatorHub cluster --type json -p '[{"op": "add", "path": "/spec/disableAllDefaultSources", "value": true}]'
-oc patch Scheduler cluster --type=merge --patch '{ "spec": { "mastersSchedulable": true } }'
-
-oc patch configs.imageregistry.operator.openshift.io cluster --type merge --patch '{"spec":{"managementState":"Managed","storage":{"emptyDir":{}}}}'
-EOF
